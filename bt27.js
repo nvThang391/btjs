3cd5ffe4019e5f6ca8a49405fc671848810ee1e1
nvThang391 <thang01684321592@gmail.com>
2024-03-08 20:50:19 +0700

    check

--- bt27.js
+++ bt27.js
@@ -29,24 +29,24 @@ formSubmit.addEventListener("submit", function (e) {
     });
   }
   removeTask(btnRemove);
-  //
-  // var btnEdit = document.querySelectorAll(".fa-pen-to-square");
-  // btnEdit.forEach((edit) => {
-  //   edit.addEventListener("click", function () {
-  //     var oldTask =
-  //       (edit.parentElement.parentElement.outerHTML = `<form id="form-addtask">
-  //     <input
-  //       type="text"
-  //       placeholder="What is the task today?"
-  //       class="input-task"
-  //       value = ${new_task}
-  //     />
-  //     <button class="btn-addtask" type="submit">Save</button>
-  //   </form>`);
-  //     var newTask =
-  //       (edit.parentElement.parentElement.outerHTML = `<div class="tasks"><p class="content">asd</p><div class="action"><i class="fa-solid fa-pen-to-square"></i>
-  //       <i class="fa-solid fa-trash"></i></div></div>`);
-  //     let updateTask = tableList.insertBefore(newTask, oldTask);
-  //   });
-  // });
+
+  var btnEdit = document.querySelectorAll(".fa-pen-to-square");
+  btnEdit.forEach((edit) => {
+    edit.addEventListener("click", function () {
+      var oldTask =
+        (edit.parentElement.parentElement.outerHTML = `<form id="form-addtask">
+      <input
+        type="text"
+        placeholder="What is the task today?"
+        class="input-task"
+        value = ${new_task}
+      />
+      <button class="btn-addtask" type="submit">Save</button>
+    </form>`);
+      var newTask =
+        (edit.parentElement.parentElement.outerHTML = `<div class="tasks"><p class="content">asd</p><div class="action"><i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-trash"></i></div></div>`);
+      let updateTask = tableList.insertBefore(newTask, oldTask);
+    });
+  });
 });
